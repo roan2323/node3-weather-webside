5e37e85519baf20a467faa0166957822f963fe7f
roan2323 <46266705+roan2323@users.noreply.github.com>
2021-08-22 14:28:15 +0200

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,18 +11,16 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = '';
   messageTwo.textContent = '';
 
-  fetch('http://localhost:3000/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          console.log(data.error);
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
 
-          messageTwo.textContent = data.error;
-        } else {
-          console.log(`Pocasi v ${data.location} je ${data.forecast}`);
-          messageOne.textContent = `Pocasi v ${data.location} je ${data.forecast}`;
-        }
-      });
-    }
-  );
+        messageTwo.textContent = data.error;
+      } else {
+        console.log(`Pocasi v ${data.location} je ${data.forecast}`);
+        messageOne.textContent = `Pocasi v ${data.location} je ${data.forecast}`;
+      }
+    });
+  });
 });
